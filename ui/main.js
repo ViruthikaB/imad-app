a7d1d17ead6e2747c01fc2727b3dbd9ca87be27c
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-03-21 21:07:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,10 @@ element.innerHTML='New value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+15;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function()
 {
-    var interval=setInterval(moveRight,50);
+    var interval=setInterval(moveRight,80);
 };
