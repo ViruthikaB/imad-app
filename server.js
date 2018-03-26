f344175f80dce32c492794b3b324f373b3a6dd7e
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-03-26 22:39:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
 var Pool=require('pg').Pool;
 var congif={
     user:'bviruthika',
@@ -101,12 +102,12 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-/*var counter=0;
+var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-*/
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
