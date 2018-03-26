77a06489f58aebf4c3859a60c113476fe1addbda
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-03-26 23:49:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/test-db',function(req,res){
         }
     });
 });
-app.get('/:articleName',function(req,res){
+/*app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
@@ -108,7 +108,7 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
