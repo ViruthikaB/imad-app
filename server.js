2820e715d202404461c6d1e3dc40e8dafb4fb4d9
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-04-01 01:52:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,6 +129,10 @@ app.get('/check-login',function(req,res){
     else{
         res.send('You are not logged in');
     }
+}); 
+app.get('/logout',function(req,res){
+    delete req.session.auth;
+    res.send('Logged out');
 });
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
