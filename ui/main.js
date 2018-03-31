cc4172347672fd3801d0bfc44d807229c830d0e4
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-04-01 01:20:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,5 +20,5 @@ submit.onclick=function(){
     console.log(username);
     console.log(password);
     request.open('POST','http://bviruthika.imad.hasura-app.io/login',true);
-    request.send(JSON.strinfigy({username: username,password:password}));
+    request.send(JSON.stringify({username: username,password:password}));
 };
