a18e40aab717a0cf570a700b90a1db9e2105256c
ViruthikaB <36499671+ViruthikaB@users.noreply.github.com>
2018-04-01 01:20:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,24 @@
-var button=document.getElementById('counter');
-button.onclick=function(){
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
-        if(request.readyState==XMLHttpRequest.DONE){
-            if(request.status==200)
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
             {
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+                console.log('user logged in');
+                alert('Logged in successfully');
+            }else if(request.status===403){
+                alert('Username/password is incorrect');
+            }else if(request.status===500){
+                alert('Something went wrong on the server');
             }
+            
         }
     };
-    request.open('GET','http://bviruthika.imad.hasura-app.io/counter',true);
-    request.send(null);
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://bviruthika.imad.hasura-app.io/login',true);
+    request.send(JSON.strinfigy({username: username,password:password}));
 };
